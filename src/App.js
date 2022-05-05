2b6c24345c1ae1702a81dd3cdd563476fdceafcf
nickrosenb <nickolas.rosenberg@gmail.com>
2022-05-05 10:47:25 -0700

    define initial components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 
+const LightBulb = () => {
+  return <></>
+}
+
+const Home = () => {
+  return <div id="home">
+    <p>class Niko &#123;</p>
+    <h1>front-end developer</h1>
+    <p>&#125;;</p>
+  </div>
+}
+
+const Skills = () => {
+  return <></>
+}
+
+const SkillSelector = () => {
+  return <></>
+}
+
+const Projects = () => {
+  return <></>
+}
+
+const ProjectSelecter = () => {
+  return <></>
+}
+
+const Work = () => {
+  return <></>
+}
+
+const WorkSelecter = () => {
+  return <></>
+}
+
+const Bio = () => {
+  return <></>
+}
+
+const About = () => {
+  return <></>
+}
+
 function App() {
   return (
     <div className="App">
